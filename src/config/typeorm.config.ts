20d2138609ad971f884c04464d292386f5570991
Taras Nurko <tarasnurko84@gmail.com>
2022-12-23 19:49:02 +0200

    project initialized

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+  useFactory: async (): Promise<TypeOrmModuleOptions> => {
+    return {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      ssl: { rejectUnauthorized: false },
+      entities: [],
+      synchronize: true,
+    };
+  },
+};

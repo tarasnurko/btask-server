7b8f6243b18ecc202ca34cc5c31dfbb1075bf797
Taras Nurko <tarasnurko84@gmail.com>
2023-01-02 15:04:35 +0200

    deleted user controller and service

--- src/core/user/user.module.ts
+++ src/core/user/user.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserController } from './user.controller';
 import { UserEntity } from './user.entity';
 import { UserRepository } from './user.repository';
-import { UserService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserRepository],
   exports: [UserRepository],
 })
 export class UserModule {}

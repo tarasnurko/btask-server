32e0d9f147145ea4782566894da27f22039b1136
Taras Nurko <tarasnurko84@gmail.com>
2023-01-15 22:34:47 +0200

    changed cors origin 9

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,23 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: 'https://btask-client.vercel.app',
-      credentials: true,
-      methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-      preflightContinue: false,
-    },
+  const app = await NestFactory.create(
+    AppModule,
+    //   {
+    //   cors: {
+    //     origin: 'https://btask-client.vercel.app',
+    //     credentials: true,
+    //     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    //     preflightContinue: false,
+    //   },
+    // }
+  );
+
+  app.enableCors({
+    origin: ['https://btask-client.vercel.app', '*'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    preflightContinue: false,
   });
 
   app.use(cookieParser());

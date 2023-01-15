7ef5922f6f3b0a10950a0660e1a362e6a3689115
Taras Nurko <tarasnurko84@gmail.com>
2023-01-15 23:21:39 +0200

    a

--- src/main.ts
+++ src/main.ts
@@ -5,31 +5,12 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    //   {
-    //   cors: {
-    //     origin: 'https://btask-client.vercel.app',
-    //     credentials: true,
-    //     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    //     preflightContinue: false,
-    //   },
-    // }
-  );
-
-  // app.enableCors({
-  // origin: ['https://btask-client.vercel.app', '*'],
-  // credentials: true,
-  // methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-  // preflightContinue: false,
-  // });
-
-  app.enableCors({
-    origin: '*',
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders:
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'https://btask-client.vercel.app',
+      credentials: true,
+      methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    },
   });
 
   app.use(cookieParser());

3134a2b4a0f9a3fe6b3813dd234d10d045395c23
Taras Nurko <tarasnurko84@gmail.com>
2023-01-15 19:29:45 +0200

    changed cors origin 4

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: true,
+      origin: '*',
       credentials: true,
     },
   });

f7544661afc7107b4ea51dc7c713abb157485dc4
Taras Nurko <tarasnurko84@gmail.com>
2023-01-15 22:51:09 +0200

    changed cors origin 12

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   // });
 
   app.enableCors({
-    origin: true,
+    origin: '*',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });

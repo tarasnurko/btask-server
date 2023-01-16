6e501f32d956681944d5b613117879c2de9a58a2
Taras Nurko <tarasnurko84@gmail.com>
2023-01-16 12:30:18 +0200

    dfdfd

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,20 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      'https://btask-client.vercel.app',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    );
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, PATCH, OPTIONS, DELETE',
+    );
+
     if (req.method === 'OPTIONS') {
       return res.status(200).json({ body: 'OK' });
     }
